910f4405341b229fe6f3ea34c7e59a015c2fcc7b
CaityHeath <caity.heath@gmail.com>
2018-12-27 22:02:54 -0800

    cleaned up code for readablitly, deleted console.logs

--- app.js
+++ app.js
@@ -5,21 +5,7 @@ const util = require('util');
 
 let read = util.promisify(fs.readFile);
 let write = util.promisify(fs.writeFile);
-
-// const alterFile = (file) => {
-//   fs.readFile( file, (err, data) => {
-//     if(err) { throw err; }
-//     let text = data.toString().toUpperCase();
-//     fs.writeFile( file, Buffer.from(text), (err, data) => {
-//       if(err) { throw err; }
-//       console.log(`${file} saved`);
-//     });
-//   });
-// };
-
 let file = process.argv.slice(2).shift();
-
-
 
 const alterFile = (file) => {
   read(file)
